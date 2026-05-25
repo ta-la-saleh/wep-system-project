64a239e92ecbaa0e0be74d1d0de88931a6f32ed4
reham-15 <rehamzuwawi15@gmail.com>
2026-05-25 23:48:12 +0300

    Add files via upload

---
+++ php update/menu.php
@@ -0,0 +1,22 @@
+<!-- زر المنيو -->
+<nav class="sub-menu-bar"><!--لإنشاء شريط التنقل الرئيسي للموقع-->
+    <div class="menu-links-container"><!--حاوية لتنظيم الروابط وتنسيقها باستخدام css-->
+        <ul class="nav-menu-links"><!-- قائمة روابط التنقل -->
+            <li><a href="Home.php" class="menu-link-item active">الرئيسية</a></li><!--رابط الصفحة الرئيسية-->
+            
+            <li class="menu-dropdown"><!-- قائمة منسدلة تحوي تخصصات الحاسب وروابطها -->
+                <a href="#" class="menu-link-item dropdown-trigger">التخصصات ▾</a>
+                <ul class="dropdown-inside-list">
+                    <li><a href="IT.php">تقنية المعلومات IT</a></li>
+                    <li><a href="CS.php">علوم الحاسب CS</a></li>
+                    <li><a href="CYBER.php">الأمن السيبراني CYBER</a></li>
+                    <li><a href="AI.php">الذكاء الاصطناعي AI</a></li>
+                    <li><a href="CE.php">هندسة الحاسب CE</a></li>
+                </ul>
+            </li>
+            
+            <li><a href="Home.php#popular-courses" class="menu-link-item">الدورات الشائعة</a></li><!-- رابط الانتقال إلى قسم الدورات الشائعة -->
+            <li><a href="courses.php" class="menu-link-item">كل الدورات</a></li><!-- رابط صفحة جميع الدورات -->
+        </ul>
+    </div>
+</nav>
